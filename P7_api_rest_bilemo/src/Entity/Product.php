e88bfe9577437bc82bdd399265d466d8680e8867
Jenny Chen <pelgrims.chenchingyi@gmail.com>
2022-01-18 09:55:58 +0100

    fix #3 update product entity property

--- P7_api_rest_bilemo/src/Entity/Product.php
+++ P7_api_rest_bilemo/src/Entity/Product.php
@@ -4,43 +4,61 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\ProductRepository;
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ * collectionOperations={"GET"},
+ * itemOperations={"GET"},
+ * normalizationContext={"groups":{"products:read"}}
+ * 
+ * 
+ *)
  * @ORM\Entity(repositoryClass=ProductRepository::class)
  */
 class Product
 {
+    public function __construct(){
+        $this->createDate=new DateTime();
+    }
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"products:read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"products:read"})
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"products:read"})
      */
     private $model;
 
     /**
      * @ORM\Column(type="string", length=500)
+     * @Groups({"products:read"})
      */
     private $description;
 
     /**
      * @ORM\Column(type="float")
+     * @Groups({"products:read"})
      */
     private $price;
 
     /**
      * @ORM\Column(type="date")
+     * @Groups({"products:read"})
      */
     private $releaseDate;
 
@@ -52,9 +70,25 @@ class Product
     /**
      * @ORM\ManyToOne(targetEntity=Brand::class, inversedBy="products")
      * @ORM\JoinColumn(nullable=false)
+     * @Groups({"products:read"})
      */
     private $Brand;
 
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $weight;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $size;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $storage;
+    
     public function getId(): ?int
     {
         return $this->id;
@@ -140,6 +174,42 @@ class Product
     public function setBrand(?Brand $Brand): self
     {
         $this->Brand = $Brand;
+
+        return $this;
+    }
+
+    public function getWeight(): ?float
+    {
+        return $this->weight;
+    }
+
+    public function setWeight(float $weight): self
+    {
+        $this->weight = $weight;
+
+        return $this;
+    }
+
+    public function getSize(): ?string
+    {
+        return $this->size;
+    }
+
+    public function setSize(string $size): self
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+
+    public function getStorage(): ?float
+    {
+        return $this->storage;
+    }
+
+    public function setStorage(float $storage): self
+    {
+        $this->storage = $storage;
 
         return $this;
     }
